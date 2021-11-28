45ecfdf99738a2c0a5d937d2e7b27b12e7442d50
camilo.garzon <camilo.garzon@servicios.in>
2021-11-27 23:52:51 -0500

    Update websocket.js

--- functions/services/websocket.js
+++ functions/services/websocket.js
@@ -12,7 +12,7 @@ exports.Connect = async(event)=>{
         
         if(!isN( event.queryStringParameters ) && !isN( event.queryStringParameters['session_token'] )){
             
-            var SesDt = await this.SessionDetail({ id:event.queryStringParameters['session_token']/*, t:'jwt'*/ });
+            var SesDt = await this.SessionDetail({ id:event.queryStringParameters['session_token'], t:'jwt' });
 
             if(SesDt.e == 'ok' && !isN(SesDt.id) && !isN(SesDt.est) && SesDt.est == 1){
 
@@ -102,63 +102,71 @@ exports.SessionDetail = async function(p=null){
 
     let fld='',
         rsp={e:'no'},
-        tableSource = `${process?.env?.DYNAMO_PRFX}-us-ses`;
+        tableSource = `${process?.env?.DYNAMO_PRFX}-us-ses`; console.log('p.id:', p.id);
 
     if(p.t == 'enc'){ 
-        fld = 'uses_enc'; 
+        fld = 'uses_enc';
     }else if(p.t == 'jwt'){
         fld = 'uses_enc'; 
-        let decoded = jwt.verify(p.id, process.env.ENCRYPT_JWT);
+        let decoded = jwt.verify(p.id, process.env.ENCRYPT_JWT); console.log('decoded:', decoded);
         p.id = decoded && decoded.data && decoded.data.session_id ? decoded.data.session_id : '';
     }else{ 
         fld = 'id_uses';
     }
 
-    var get = await DYNAMO.query({
-                TableName : tableSource,
-                KeyConditionExpression: "#id=:idv",
-                ExpressionAttributeNames:{ "#id": "uses_enc" },
-                ExpressionAttributeValues: { ":idv": p.id },
-                Limit: 1
-            }).promise(); console.log('get query:',get);
-    
-    if(!get?.Items[0]){
+    try{
         
-        var get = await DYNAMO.scan({
-                TableName: tableSource,
-                FilterExpression: '#id=:idv',
-                ExpressionAttributeValues: {
-                    ':idv': p.id,
-                },
-                Limit: 1
-            }).promise(); console.log('get scan:',get);
+        var get = await DYNAMO.query({
+                    TableName : tableSource,
+                    IndexName: 'uses_enc-index',
+                    KeyConditionExpression: 'uses_enc = :idv',
+                    ExpressionAttributeValues: { ":idv": p.id },
+                    Limit: 1
+                }).promise(); console.log('get query:',get);
+        
+        if(!get?.Items[0]){
             
-    }
-
-    if(!get?.Items[0]){
-
-        var get = await DBGet({
-                            q: `SELECT id_uses, uses_enc, uses_est FROM `+DBSelector('us_ses')+` WHERE ${fld}=? LIMIT 1`,
-                            d:[ p.id ]
-                        });
-
-    }
-
-    if(get){
-        
-        rsp.e = 'ok';
-
-        if(!isN(get[0])){
-            rsp.id = get[0].id_uses;
-            rsp.enc = get[0].uses_enc;
-            rsp.est = get[0].uses_est;
+            var get = await DYNAMO.scan({
+                    TableName: tableSource,
+                    IndexName: 'uses_enc-index',
+                    FilterExpression: 'uses_enc = :idv',
+                    ExpressionAttributeValues: { ':idv': p.id },
+                    Limit: 1
+                }).promise(); console.log('get scan:',get);
+                
         }
 
-    }else {
+        if(!get?.Items[0]){
 
-        rsp['w'] = 'No ID result';
+            var get = await DBGet({
+                                q: `SELECT id_uses, uses_enc, uses_est FROM `+DBSelector('us_ses')+` WHERE ${fld}=? LIMIT 1`,
+                                d:[ p.id ]
+                            });
+
+        }
+
+        if(get){
+            
+            rsp.e = 'ok';
+
+            if(!isN(get[0])){
+                rsp.id = get[0].id_uses;
+                rsp.enc = get[0].uses_enc;
+                rsp.est = get[0].uses_est;
+            }
+
+        }else {
+
+            rsp.w = 'No ID result';
+
+        }
+
+    }catch(err){
+
+        rsp = { status:'failed', error:err };
 
     }
+
 
     return rsp;
 
